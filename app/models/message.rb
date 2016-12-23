921f447e2c5b91aca4b50274fa08775a1be2542e
lillccian <lillccian@gmail.com>
2016-12-23 15:23:11 +0800

    add where 7 days

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,8 +7,11 @@ class Message < ApplicationRecord
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
   end
-
+  # time = params[:days].to_i
   scope :pending, ->{ where( :status => "pending" ) }
   scope :completed, ->{ where( :status => "completed" ) }
 
+  def self.within_days(time)
+      where( "created_at >= ?",time.days )
+  end
 end
